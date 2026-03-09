085a5d141c32a391e748770dea193ccb2a4f4415
dash1c <sonfel@yandex.ru>
2026-03-09 23:55:08 +0300

    поправила файл с людьми 1

--- rent_bot_fixed.py
+++ rent_bot_fixed.py
@@ -18,7 +18,7 @@ storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 
 # Файл для хранения данных
-DATA_FILE = "/data/rent_data.json" 
+DATA_FILE = "/app/data/rent_data.json" 
 
 # 📢 ТЕСТОВЫЙ РЕЖИМ: True = минуты, False = дни
 TEST_MODE = False
